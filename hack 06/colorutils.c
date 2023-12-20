69b6dd53dbc14a4627038323617097f79ca8cc21
hamzashamshad136020 <hamzashamshad136020@gmail.com>
2023-12-20 20:02:15 +0800

    rgb to 0 to 1 scale

--- hack 06/colorutils.c
+++ hack 06/colorutils.c
@@ -5,6 +5,10 @@
 int rgbToCMYK(int r, int g, int b, double *c, double *m, double *y, double *k)
 {
 
+int r1=r/255;
+int g1=g/255;
+int b1=b/255;
+int k=
 
 
 
@@ -13,6 +17,4 @@ int rgbToCMYK(int r, int g, int b, double *c, double *m, double *y, double *k)
 
 
 
-
-    
 }
