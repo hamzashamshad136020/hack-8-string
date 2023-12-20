ed94b612eaa4d9a35be16c427964a20fcab4cd36
hamzashamshad136020 <hamzashamshad136020@gmail.com>
2023-12-20 20:58:27 +0800

    correction

--- hack 06/colorutils.c
+++ hack 06/colorutils.c
@@ -25,10 +25,10 @@ int rgbToCMYK(int r, int g, int b, double *c, double *m, double *y, double *k)
         int c1 = ((1 - r1 - k1) / (1 - k1));
         int m1 = ((1 - g1 - k1) / (1 - k1));
         int y1 = ((1 - b1 - k1) / (1 - k1));
-        c = &c1;
-        m = &m1;
-        y = &y1;
-        k = &k1;
+        *c = c1;
+       *m = m1;
+        *y = y1;
+        *k = k1;
 
         return 0;
     }
@@ -48,9 +48,9 @@ int cmykToRGB(double c, double m, double y, double k, int *r, int *g, int *b)
         int g1 = 255 * (1 - m) * (1 - k);
         int b1 = 255 * (1 - y) * (1 - k);
 
-        r = &r1;
-        g = &g1;
-        b = &b1;
+        *r = r1;
+        *g = g1;
+        *b = b1;
         return 0;
     }
 }
