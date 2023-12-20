75b68339924c2d3a581b749ea49c7efd8aebe39f
hamzashamshad136020 <hamzashamshad136020@gmail.com>
2023-12-20 20:05:22 +0800

    add max function

--- hack 06/colorutils.c
+++ hack 06/colorutils.c
@@ -1,5 +1,13 @@
 #include <colorutils.h>
 
+int max(int x, int y, int z)
+{
+  int m = x > y ? x : y;
+  m = m > z ? m : z;
+  return m;
+}
+
+
 
 
 int rgbToCMYK(int r, int g, int b, double *c, double *m, double *y, double *k)
@@ -8,13 +16,7 @@ int rgbToCMYK(int r, int g, int b, double *c, double *m, double *y, double *k)
 int r1=r/255;
 int g1=g/255;
 int b1=b/255;
-int k=
-
-
-
-
-
-
+int k=1- max(r1,g1,b1);
 
 
 }
--- hack 06/colorutils.h
+++ hack 06/colorutils.h
@@ -1,2 +1,3 @@
 
 int rgbToCMYK(int r, int g, int b, double *c, double *m, double *y, double *k);
+int max(int x, int y, int z);
