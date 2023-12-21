ecc8a760525cc004d641f1ada1c420ba96a83e12
hamzashamshad136020 <hamzashamshad136020@gmail.com>
2023-12-21 20:50:53 +0800

    removeCharCopy function

--- strings hack/string_utils.c
+++ strings hack/string_utils.c
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-
 void replaceChar(char *s, char oldChar, char newChar)
 {
     while (*s != '\0')
@@ -16,52 +14,59 @@ void replaceChar(char *s, char oldChar, char newChar)
     }
 }
 
-
-
-char * replaceCharCopy(const char *s, char oldChar, char newChar)
+char *replaceCharCopy(const char *s, char oldChar, char newChar)
 {
-int lenght=strlen(s);
-char *s1=(char *)malloc(lenght +1);
-for (int i = 1; i < lenght; i++,s++)
-{
-    if (s==oldChar)
+    int lenght = strlen(s);
+    char *s1 = (char *)malloc(lenght + 1);
+    for (int i = 1; i < lenght; i++, s++)
     {
-        s1[i]=newChar;
+        if (s == oldChar)
+        {
+            s1[i] = newChar;
+        }
+
+        s1[i] = s;
     }
-    
-    s1[i]=s;
+
+    s1[lenght + 1] = '\0';
+
+    return s1;
 }
-
-
-s1[lenght+1]='\0';
-
-
-}
-
-
 
 void removeChar(char *s, char c)
 {
-int lenght=strlen[s];
-while(s[i]!='\0')
-{
-if (s[i]==c)
-{
-   s[i] =s[i+1]
-}
+    int lenght = strlen[s];
+    while (s[i] != '\0')
+    {
+        if (s[i] == c)
+        {
+            s[i] = s[i + 1]
+        }
+
+        i++;
+    }
+
+    char *removeCharCopy(const char *s, char c)
+    {
+        int lenght = strlen(s);
+   
+      char *newcopy = (char *)malloc(lenght);
 
 
-    i++;
-}
+      for (int i = 0; i < lenght; i++)
+      {
+        if (*s != c)
+        {
+*newcopy=*s;
+new copy++;
+s++;
+      }
+      
+      
+s++;
 
 
+    }
+newcopy[lenght+1]='\0';
 
-
-
-
-
-
-
-
-
-}
+    }
